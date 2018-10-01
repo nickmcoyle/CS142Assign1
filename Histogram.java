dda829483ffa60e483ec93ee8426b7678a0dd7d7
Nick <nickmcoyle@gmail.com>
2018-10-01 00:13:19 -0700

    fixed some inconsistent line spacing

--- Histogram.java
+++ Histogram.java
@@ -10,14 +10,14 @@
 public class Histogram{ 
     public static final String TITLE = "Presidential Elections";
     public static final int SCREENWIDTH = 80;
+    public static final String LEFTMARGIN = "       ";
     public static final char TOTALELIGIBLE = 'T';
     public static final char REGISTEREDVOTERS = 'R';
     public static final char VOTESCAST = 'V';
     public static final int SCALE = 100000;
     public static final int STEP = 1000000;
        
-    public static void main(String[] args) {      
-        
+    public static void main(String[] args) {       
         printHeader(TITLE);
         
         createBar(2000, 4368000, TOTALELIGIBLE, SCALE);
@@ -85,7 +85,7 @@ public class Histogram{
      *  @param scale the units of the y axis
      */
     public static void createBar(int barTitle, int barValue, char dataChar, int scale) {
-        System.out.print(barTitle + " |");
+        System.out.print("  " + barTitle + " |");
         
         int numBars = barValue/scale;
         for(int i = 0; i<numBars; ++i) {
@@ -105,7 +105,7 @@ public class Histogram{
      *  @param scale the units of the y axis
      */
     public static void createBar(int barValue, char dataChar, int scale) {
-        System.out.print("     |");
+        System.out.print(LEFTMARGIN + "|");
         
         int numBars = barValue/scale;
         for(int i = 0; i<numBars; ++i) {
@@ -114,15 +114,15 @@ public class Histogram{
     }
     
     /**
-     *   Computes the percentage difference between the bar above this bar
+     *  Computes the percentage difference between the bar above this bar
      *   
-     *   @param total is the maximum quantity possible
-     *   @param actual is the quantity that is either the number that was registered or that voted out of the possible 100%
+     *  @param total is the maximum quantity possible
+     *  @param actual is the quantity that is either the number that was registered or that voted out of the possible 100%
      */
     public static void createPercentage(int total, int actual) {
         float percentage = ((float)actual/(float)total)*100.00f;       
                
-        System.out.printf("   %.2f", percentage);
+        System.out.printf("     %.2f", percentage);
         System.out.print("%");
         
         System.out.println();
@@ -135,7 +135,7 @@ public class Histogram{
      *  @param step the amount each value increases for every 10 points on the x-axis
      */
     public static void createAxis(int step) {
-        System.out.print("     |");
+        System.out.print(LEFTMARGIN + "|");
         
         for(int i = 2; i < SCREENWIDTH/10; ++i) {
             for(int j = 1; j < 10; ++j) {
@@ -145,7 +145,7 @@ public class Histogram{
         }
 
         System.out.println();
-        System.out.print("     " + 0 + "      ");
+        System.out.print(LEFTMARGIN + 0 + "      ");
         
         for(int i = 1; i <= SCREENWIDTH/10-2; ++i) {            
             System.out.print(i*step);            
@@ -159,6 +159,6 @@ public class Histogram{
      *  Prints an empty row to the chart for style
      */
     public static void emptyRow() {
-        System.out.println("     |");
+        System.out.println(LEFTMARGIN + "|");
     }
 }
